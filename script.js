b379ca64499a9c951d3252649de74841ecc93c22
shubhbafna24 <shubhbafna24@gmail.com>
2023-12-30 22:12:19 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//console.log("Hello djnfjsn");
+//let userName = "Shubh"
+// alert("Hellp");
+// let hello="Abcd";
+// let abc=`Shubh ${hello}`;
+
+// const user={
+     
+// }
+
+// function introduceMe(){
+//     console.log("Hello");
+//     console.log("I am Shubh");
+// }
+// introduceMe();
+
+//console.log(userName);
+const userName='Anurag'
+console.log(userName);
+
+//console.log(userName1);
+let userName1='Anurag2'
+console.log(userName1);
+
+console.log(userName2);
+var userName2='Anurag3'  
+console.log(userName2);
